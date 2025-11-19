519c2b15f28d9318f05b82346e6d6f0dd5e7ed65
Alireza Akbarzadeh <82927248+alireza-akbarzadeh@users.noreply.github.com>
2025-11-19 10:46:02 +0330

    feat: create not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,5 @@
-'use clinent';
+'use client';
 import Link from 'next/link';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: '404 - Page Not Found',
-  description: 'The page you are looking for could not be found.',
-};
 
 export default function NotFound() {
   return (
